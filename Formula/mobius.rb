0ed48a0d60fed428adeb0258d27b36e5de3e3423
Curtis Myzie <curtis.myzie@gmail.com>
2026-07-21 01:22:57 -0400

    Update mobius to 0.0.57

--- Formula/mobius.rb
+++ Formula/mobius.rb
@@ -1,26 +1,26 @@
 class Mobius < Formula
   desc "CLI for the Mobius agent automation platform"
   homepage "https://www.mobiusops.ai/"
-  version "0.0.56"
+  version "0.0.57"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/deepnoodle-ai/mobius/releases/download/v0.0.56/mobius-darwin-arm64"
-      sha256 "3cf47b14fbb7542b643255a3b61384bf7aead8d7124abfa61fa41ebfb34fa9eb"
+      url "https://github.com/deepnoodle-ai/mobius/releases/download/v0.0.57/mobius-darwin-arm64"
+      sha256 "13bd11274121a61845f9384f5a7c9d3ecd4c3a1b909a8d1208adc3f2c47041eb"
     else
-      url "https://github.com/deepnoodle-ai/mobius/releases/download/v0.0.56/mobius-darwin-amd64"
-      sha256 "2ac380d55dbe10800df62941bee03b5b968eea8312b6ab185f4c051f080151e0"
+      url "https://github.com/deepnoodle-ai/mobius/releases/download/v0.0.57/mobius-darwin-amd64"
+      sha256 "da29c977139de0290c78cae551243bdcf7987330b5e60b1d0c2913c431a23c7d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/deepnoodle-ai/mobius/releases/download/v0.0.56/mobius-linux-arm64"
-      sha256 "5d50b26a0857fcf0d8e89ec92ae6b99ef98f92b8528106b07ea19a90b77ac04c"
+      url "https://github.com/deepnoodle-ai/mobius/releases/download/v0.0.57/mobius-linux-arm64"
+      sha256 "b664a13c5a6332f254508d1d641ab917eda6ac0ce3aa59422edc33067e2e85db"
     else
-      url "https://github.com/deepnoodle-ai/mobius/releases/download/v0.0.56/mobius-linux-amd64"
-      sha256 "7292d6d11cf580bb945d93623b25749c73019361fa91f79bc07e2cbadd56f03d"
+      url "https://github.com/deepnoodle-ai/mobius/releases/download/v0.0.57/mobius-linux-amd64"
+      sha256 "eabcea5177d9424cace6d0b82795b1680d758e9208af045f57580c82f961b0a6"
     end
   end
 
@@ -30,6 +30,6 @@ class Mobius < Formula
   end
 
   test do
-    assert_match "0.0.56", shell_output("#{bin}/mobius --version 2>&1")
+    assert_match "0.0.57", shell_output("#{bin}/mobius --version 2>&1")
   end
 end
